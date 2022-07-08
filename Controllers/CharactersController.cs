e63248349aacf70cae9f21b0cad0a3862d552eec
Charles Neukuckatz <charneuk@gmail.com>
2022-07-08 16:06:42 -0500

    DBContext

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -1,22 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    public class CharactersController:ControllerBase
+    public class CharactersController : ControllerBase
     {
-        [HttpGet("api/characters")]
-        public JsonResult GetCharacters()
-        {
-            return new JsonResult(
-                new List<object>
-                {
-                    new { id = 1, name =  "Charmander" },
-                    new { id = 2, name = "Pikachu"},
-                    new { id = 3, name = "Charmeleon"}
-                });
+        
+        private readonly SuperSmashDBContext _context; 
+
+        public CharactersController(SuperSmashDBContext context) => _context = context; 
+
+        [HttpGet]
+        public async Task <IEnumerable<Character>> Get()
+        
+            => await _context.Characters.ToListAsync();
+        
+        
+            
                 
                 
-        }
+        
     }
 }
--- Models/Character.cs
+++ Models/Character.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication2.Models
 {
     public partial class Character
     {
         public int Id { get; set; }
+        [Required]
         public string CharacterName { get; set; } = null!;
     }
 }
