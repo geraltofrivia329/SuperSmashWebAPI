03d5895406b541b463c8a53c818be41d6784d619
Charles Neukuckatz <charneuk@gmail.com>
2022-07-18 15:47:03 -0500

    Added StageController.cs

---
+++ Controllers/StagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+
+    public class StagesController : ControllerBase
+        {
+
+            private readonly SuperSmashDBContext _context;
+
+            public StagesController(SuperSmashDBContext context) => _context = context;
+
+            [HttpGet]
+            public async Task<IEnumerable<Stage>> Get()
+
+                => await _context.Stages.ToListAsync();
+        }
+}
